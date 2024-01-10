cc8c8acd0619f3942b5cfadb7757394aebedf29b
IDragonThunderI <andrey.voltr@yandex.ru>
2024-01-10 22:06:32 +0400

    Добавьте файлы проекта.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <omp.h>
+
+int main() {
+	system("chcp 1251 > nul");
+
+#ifndef _OPENMP
+    std::cout << "############################################" << std::endl;
+    std::cout << "OpenMP не поддерживается вашим компилятором!" << std::endl;
+    std::cout << "############################################" << std::endl;
+#endif // !_OPENMP
+
+#ifdef _OPENMP
+    std::unordered_map<unsigned, std::string> map {
+        {199810,"1.0"},
+        {200203,"2.0"},
+        {200505,"2.5"},
+        {200805,"3.0"},
+        {201107,"3.1"},
+        {201307,"4.0"},
+        {201511,"4.5"},
+        {201811,"5.0"},
+        {202011,"5.1"},
+        {202111,"5.2"}
+    };
+    std::cout << "#########################" << std::endl;
+    std::cout << "Текущая версия OpenMP " << map.at(_OPENMP) << std::endl;
+    std::cout << "#########################" << std::endl << std::endl;
+
+    std::cout << "Количество ядер процессора: " << omp_get_num_procs() / 2 << std::endl;
+#endif // _OPENMP
+    return 0;
+}
---
+++ Task2/Task2.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <random>
+#include <vector>
+#include <omp.h>
+
+#define matrixType std::vector<std::vector<int>>
+//#define DEBUG
+
+// Заполнение матрицы рандомными числами
+void fillMatrix(matrixType& matrix, std::mt19937& randomGenerator) {
+	for (auto& row : matrix) {
+		for (auto& column : row) {
+			std::uniform_int_distribution<int> distribution(-10, 10);
+			column = distribution(randomGenerator);
+		}
+	}
+}
+
+// Вывод матрицы на экран
+void printMatrix(const matrixType& matrix) {
+	for (auto& row : matrix) {
+		for (auto& column : row) {
+			std::cout << column << "\t";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+// Перемножение матриц
+matrixType matrixMultiplication(const matrixType& matrixA, const matrixType& matrixB, const bool& parallelMode = false) {
+	int matrixSize = matrixA.size();
+	matrixType result(matrixSize, std::vector<int>(matrixSize, 0));
+
+	// Представление матрицы
+	/*												{matrix}
+					 [row]				             [row]							 [row]
+		{[(column), (column), (column)], [(column), (column), (column)], [(column), (column), (column)]}
+
+	*/
+
+	#pragma omp parallel if (parallelMode)
+	{
+		#ifdef DEBUG
+		#pragma omp for ordered
+		#endif // DEBUG
+
+		#ifndef DEBUG
+		#pragma omp for
+		#endif // !DEBUG
+
+		for (int row = 0; row < matrixSize; row++) {
+			#ifdef DEBUG
+			#pragma omp ordered
+			#endif // DEBUG
+
+			for (int column = 0; column < matrixSize; column++) {
+				for (int position = 0; position < matrixSize; position++) {
+					result.at(row).at(column) += matrixA.at(row).at(position) * matrixB.at(position).at(column);
+					#ifdef DEBUG
+					std::cout << "Поток " << omp_get_thread_num() << " перемножил элементы A[" << row << "][" << position << "]" <<
+						" и " << "B[" << position << "][" << column << "] и прибавил к C[" << row << "][" << column << "]" <<
+						" (" << matrixA.at(row).at(position) << " * " << matrixB.at(position).at(column) << " + " <<
+						result.at(row).at(column) - matrixA.at(row).at(position) * matrixB.at(position).at(column) << " = " <<
+						result.at(row).at(column) << ")" << std::endl;
+					#endif // DEBUG
+				}
+			}
+		}
+	}
+	return result;
+}
+
+int main() {
+	system("chcp 1251 > nul");
+	std::mt19937 randomGenerator;
+	std::random_device device;
+	randomGenerator.seed(device());
+
+	size_t matrixSize;
+	std::cout << "Введите размерность матрицы: ";
+	std::cin >> matrixSize;
+	matrixType matrixA(matrixSize, std::vector<int>(matrixSize));
+	matrixType matrixB(matrixSize, std::vector<int>(matrixSize));
+	matrixType matrixC(matrixSize, std::vector<int>(matrixSize));
+
+	fillMatrix(matrixA, randomGenerator);
+	fillMatrix(matrixB, randomGenerator);
+
+	if (matrixSize <= 5) {
+		std::cout << "matrixA: " << std::endl;
+		printMatrix(matrixA);
+		std::cout << "matrixB: " << std::endl;
+		printMatrix(matrixB);
+	}
+
+	// При умножении квадратных матриц размера nxn последовательным способом количество выполненных операций имеет порядок O(n^3)
+	std::cout << "Поэлементное перемножение:" << std::endl;
+	double start = omp_get_wtime();
+	matrixC = matrixMultiplication(matrixA, matrixB);
+	double finish = omp_get_wtime();
+	std::cout << "Время выполнения: " << finish - start << std::endl;
+
+	std::cout << std::endl;
+
+	// При умножении квадратных матриц размера nxn ленточным способом по строкам количество выполненных операций имеет порядок O(n)
+	std::cout << "Ленточное перемножение по строкам:" << std::endl;
+	start = omp_get_wtime();
+	matrixC = matrixMultiplication(matrixA, matrixB, true);
+	finish = omp_get_wtime();
+	std::cout << "Время выполнения: " << finish - start << std::endl;
+
+	if (matrixSize <= 5) {
+		std::cout << "matrixC = matrixA * matrixB: " << std::endl;
+		printMatrix(matrixC);
+	}
+
+	return 0;
+}
